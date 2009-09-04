632de60f33a45b4d159bbdd55f2c30eaa75665a4
kla <la.kien@gmail.com>
2009-09-04 19:44:23 -0400

    updated readme

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 Ruby library for querying game information from Heroes of Newerth (http://www.heroesofnewerth.com/).
 
-...
+## Example Usage ##
+
+character = Honsolo::Character.find("teeto")
+puts character.inspect
+
+guild = Honsolo::Clan.find(character.clan_info.clan_id)
+puts guild.inspect
--- lib/honsolo/character.rb
+++ lib/honsolo/character.rb
@@ -3,14 +3,14 @@ module Honsolo
     class << self
       def find_id(name)
         name = name.downcase
-        data = Honsolo.post("f" => "nick2id", "nickname[0]" => name)
+        data = Honsolo.post("f" => "nick2id", "nickname[]" => name)
         raise Honsolo::CharacterNotFound, name unless data.is_a?(Hash) && data.has_key?(name)
         data[name].to_i
       end
 
       def find(name_or_id)
         id = name_or_id.is_a?(Fixnum) ? name_or_id : find_id(name_or_id)
-        data = Honsolo.post("f" => "get_all_stats", "account_id[0]" => id)
+        data = Honsolo.post("f" => "get_all_stats", "account_id[]" => id)
         raise Honsolo::CharacterNotFound, name_or_id if data["all_stats"].is_a?(Array) && data["all_stats"].empty?
 
         character = Honsolo.to_ostruct(data["all_stats"],id)
